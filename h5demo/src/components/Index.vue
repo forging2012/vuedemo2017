d8610eb245ee2da74a78b48093104773f73220da
WIN-01704201232\Administrator <869710179@qq.com>
2017-08-08 13:42:01 +0800

    内有相关的截图

---
+++ h5demo/src/components/Index.vue
@@ -0,0 +1,114 @@
+<template>
+    <div>
+      <nav>
+        <ul>
+          <li :class='{"router-link-active":navIndex==index}' v-for="(item,index) in navList" @click="toggle(index)">
+            <router-link :to="item.path">
+              <i :class='[item.icon]'></i>
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="conter">
+        <router-view></router-view>
+      </div>
+    </div>
+</template>
+<script type="text/ecmascript-6">
+    export default {
+      name:'Index',
+      data() {
+        return {
+          screenWidth: document.body.clientWidth,
+          isCollapse: false,
+          navIndex:0,
+          navList:[
+            {
+              id:1,
+              span:'首页',
+              path:'/BaseIndex',
+              icon:'iconfont icon-home'
+            },
+            {
+              id:2,
+              span:'博客',
+              path:'/ArticleIndex',
+              icon:'iconfont icon-article'
+            },
+            {
+              id:3,
+              span:'分享',
+              path:'/ShareIndex',
+              icon:'iconfont icon-share'
+            },
+            {
+              id:4,
+              span:'地球',
+              path:'/EarthIndex',
+              icon:'iconfont icon-diqiu'
+            },
+            {
+              id:5,
+              span:'关于',
+              path:'/AboutIndex',
+              icon:'iconfont icon-guanyu'
+            },
+          ]
+        };
+      },
+
+      methods: {
+        toggle(index){
+         this.navIndex=index
+        },
+        updatePaged(){
+//          alert(window.location.href)
+
+        }
+      },
+      components:{
+      },
+
+      mounted () {
+        const that = this
+        window.onresize = () => {
+          return (() => {
+            window.screenWidth = document.body.clientWidth
+            that.screenWidth = window.screenWidth
+          })()
+        };
+      },
+      updated(){
+//        alert(this.navIndex)
+      }
+
+    }
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../src/comm/css/index.styl"
+  .conter
+    float left
+    width 80%
+    padding 12px
+    box-sizing border-box
+  nav
+    width 15%
+    text-align center
+    float left
+    ul
+      padding 12px
+      li
+        padding 0!important
+        font-size 40px
+        line-height 60px
+        a
+          text-decoration none
+          i.iconfont
+            font-size inherit
+/*nav切换时*/
+.router-link-active
+  color #f66
+  a
+    text-decoration none
+
+</style>
---
+++ h5demo/src/components/subcomponents/article/article1.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <span>
+        文章序列id
+        文章{{$route.params.newsId}}
+        {{abc2}}
+      </span>
+    </div>
+</template>
+<script type="text/ecmascript-6">
+  import Vue from 'vue'
+  var eventBus = new Vue();
+    export default {
+      name:'article1',
+      data(){
+        return{
+          abc2:''
+        }
+      },
+      methods:{
+        addclis(){
+          eventBus.$on('articles', function (id) {
+            this.abc2=id
+            this.abc2='aaa'
+          }.bind(this))
+        }
+      },
+      mounted(){
+        this.addclis()
+      }
+    }
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
